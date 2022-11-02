d53d797176f55d6f1c3f450a6c1816a5ed83570f
unknown <tanasun696@gmail.com>
2022-11-02 16:46:59 -0400

    added opensea listing endpoint

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,8 @@
 const config = {
   SeaportAddress: "0x00000000006c3852cbEf3e08E8dF289169EdE581",
+  TestnetEndpoint: "https://testnets-api.opensea.io/v2/orders/goerli/seaport",
+  MainnetEndpoint: "https://api.opensea.io/v2/orders/ethereum/seaport",
+  SeaportEndpoint: "https://testnets-api.opensea.io/v2/orders/goerli/seaport",
 };
 
 export default config;
